e2ced6d45417148c842700aad416d2af53a6b763
Crystal Shamo <crystal.shamo@gmail.com>
2023-10-24 21:48:04 -0400

    commit

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -18,6 +18,7 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.myapplication.DataModel.ClosetItem;
+import com.example.myapplication.DataModel.DataConverter;
 import com.example.myapplication.DataModel.ItemDAO;
 import com.example.myapplication.DataModel.UserDatabase;
 
@@ -26,6 +27,7 @@ import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
+    private UserDatabase closetDatabase;
     private static final int pic_id = 123;
     Button camera_open_id;
 
@@ -46,6 +48,8 @@ public class MainActivity extends AppCompatActivity {
         color = findViewById(R.id.colorOfItem);
         season = findViewById(R.id.seasonOfItem);
         itemDAO = UserDatabase.getDBInstance(this).itemDAO();
+        closetDatabase = Room.databaseBuilder(getApplicationContext(),
+                UserDatabase.class, "closet-database").build();
         camera_open_id.setOnClickListener(v -> {
             // Create the camera_intent ACTION_IMAGE_CAPTURE it will open the camera for capture the image
             Intent camera_intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -62,40 +66,36 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-   protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         // Match the request 'pic id with requestCode
         if (requestCode == pic_id) {
             // BitMap is data structure of image file which store the image in memory
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            bmpImage = (Bitmap) data.getExtras().get("data");
             // Set the image in imageview for display
-            imageView.setImageBitmap(photo);
+            imageView.setImageBitmap(bmpImage);
         } }
 
-        public void saveItem (View view){
-            if (color.getText().toString().isEmpty()
-                    || season.getText().toString().isEmpty()
-                    ||  bmpImage ==null
-                   ) {
-                Toast.makeText(
-                        this,
-                        "User Data is Missing",
-                        Toast.LENGTH_SHORT
-                ).show();
-            } else {
-                ClosetItem closetItem = new ClosetItem();
-                closetItem.setColorOfItem(color.getText().toString());
-                closetItem.setSeasonOfItem(season.getText().toString());
-                itemDAO.insertClosetItem(closetItem);
-                Toast.makeText(
-                        this,
-                        "Insertion successful",
-                        Toast.LENGTH_SHORT
-                ).show();
-            } } }
-
-
-
-
-
+    public void saveItem (View view){
+        if (color.getText().toString().isEmpty()
+                || season.getText().toString().isEmpty()
+                || bmpImage == null
+        ) {
+            Toast.makeText(
+                    this,
+                    "User Data is Missing",
+                    Toast.LENGTH_SHORT
+            ).show();
+        } else {
+            ClosetItem closetItem = new ClosetItem();
+            closetItem.setColorOfItem(color.getText().toString());
+            closetItem.setSeasonOfItem(season.getText().toString());
+            closetItem.setImage(DataConverter.convertImage2ByteArray(bmpImage));
+            itemDAO.insertClosetItem(closetItem);
+            Toast.makeText(
+                    this,
+                    "Insertion successful",
+                    Toast.LENGTH_SHORT
+            ).show();
+        } } }
 
